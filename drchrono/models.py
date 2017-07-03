3d6b6398e681391f3c94bb2b17b1d4dadf645bb8
Vipul Munot <vipmunot@gmail.com>
2017-07-03 06:19:53 -0700

    adding models

--- drchrono/models.py
+++ drchrono/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from datetime import datetime
+
 # Create your models here.
+
+
+class Patient(models.Model):
+    first_name = models.CharField(default="",max_length=20);
+    last_name = models.CharField(default="",max_length=20);
+    doctor = models.PositiveIntegerField(default=-1);
+    gender = models.CharField(default="Male",max_length=60);
+    date_of_birth = models.CharField(default=str(datetime.now()), max_length= 10);
+
+
+# python manage.py makemigrations
+# python manage.py migrate
+
+class User(models.Model):
+    user_name = models.CharField(max_length=20);
+    access_token = models.CharField(max_length=100);
