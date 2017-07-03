5102d98332875988c1962908712d0f74f8547014
Vipul Munot <vipmunot@gmail.com>
2017-07-03 06:21:20 -0700

    updating url files

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -1,11 +1,17 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.views.generic import TemplateView
-
+from django.contrib import  admin
 import views
+import settings
 
+admin.autodiscover();
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 
-    url(r'', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^$', views.get_access,name = "cId"),
+    url(r'^home/$' , views.get_home, name='home'),
+    url(r'^home/wishpatient/(?P<id>\d+)/$', views.wishpatient,name = "wishPatient"),
+    url(r'^home/sendemail',views.sendEmail),
+    url(r'^admin/',include(admin.site.urls)),
+
 ]
